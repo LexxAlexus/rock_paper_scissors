01f4e9ffd05a462569a4efc053216bda3da5aadf
LexxAlexus <107062566+LexxAlexus@users.noreply.github.com>
2023-08-17 09:19:09 -0500

    Start a loop over game

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 "use strict"
 //Computer's random pick function
 let getComputerChoice = function() {
-    let computerAnswer = Math.floor(Math.random() * 3); 
+    let computerAnswer = Math.floor(Math.random() * 3);
 
-    switch (computerAnswer) {
-    case 0:
-        console.log('rock');
-        break;
-    case 1:
-        console.log('paper');
-        break;
-    case 2:
-        console.log('scissors');
-}
+    switch(computerAnswer) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
 };
-getComputerChoice();
+//console.log(getComputerChoice());
 
 
 //Player Selection 
@@ -22,7 +20,31 @@ let userSelection = prompt('Choose: rock, paper, scissors');
 
 //Lowercase Selection
 let userResult = userSelection.toLowerCase();
-console.log(userResult);
+//console.log(userResult);
+
+
+const gamePLay = function(getComputerChoice, userResult) {
+    if(getComputerChoice === userResult) {
+        return 'No winner! Same answers';
+    } else if(getComputerChoice === 'rock' && userResult === 'paper') {
+        return 'Player wins! Paper beats rock!';
+    } else if(getComputerChoice === 'paper' && userResult === 'rock') {
+        return 'Computer wins! Paper beats rock!';
+    } else if(getComputerChoice === 'scissors' && userResult === 'paper') {
+        return 'Computer wins! Scissors beats paper';
+    } else if(getComputerChoice === 'scissors' && userResult === 'rock') {
+        return 'Player wins! Rock beats scissors!';
+    } else if(getComputerChoice === 'rock' && userResult === 'scissors') {
+        return 'Computer wins! Rock beats scissors';
+    } else if(getComputerChoice === 'paper' && userResult === 'scissors') {
+        return 'Player wins! Scissors beats paper!';
+    } else {
+        return 'game';
+    }
+};
+
+const begin = gamePLay(getComputerChoice(), userResult);
+console.log(begin);
 
 
 /* let userSelection = prompt('Hello, Choose between rock, paper, scissors?');
