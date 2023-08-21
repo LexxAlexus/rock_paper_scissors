5cac7005335f51d82f99e31885e09647c814e6ed
LexxAlexus <107062566+LexxAlexus@users.noreply.github.com>
2023-08-21 08:52:08 -0500

    Contiune

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 "use strict"
-//Computer's random pick function
-let getComputerChoice = function() {
-    let computerAnswer = Math.floor(Math.random() * 3);
+//Array of choices for computer function and User Function/Validate
+let gameChoices = ['rock', 'paper', 'scissors'];
+/* 
+//Function to play game
+function playGame() {
+    computerChoice();
+    playerChoice();
+}
 
-    switch(computerAnswer) {
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors';
-    }
+//Function for computer random choose from gameChoices array
+function computerChoice() {
+    return gameChoices[(Math.floor(Math.random() * gameChoices.length))];
+}
+//console.log(computerChoice());
+
+let playerChoice = function() {
+        //Get player response
+        let response = prompt('Welcome! Please choose rock, paper, scissors');
+
+        //When response is empty and player clicks cancel or okay
+        while(response === null || response === '') {
+            response = prompt('Welcome! Please choose rock, paper, scissors');
+        };
+        //Lowercase all input
+        response = response.toLowerCase();
+
+        //Function that checks player input for correct response from array
+        let check = validate(response);
+        //If function is false from choices prompt the question again 
+        while(check === false) {
+            response = prompt('Only options rock, paper, scissors. Check spelling if needed.');
+
+            //Inside the previous while loop to check if empty response/canceled
+            while(response === null || response === '') {
+            response = prompt('Welcome! Please choose rock, paper, scissors'); 
+            }
+            //when function is true make lowercase
+            check = validate(response);
+            response = response.toLowerCase();
+        
+        }
 };
-//console.log(getComputerChoice());
 
+//Function to check correct responses from array
+function validate (userResponse) {
+    return gameChoices.includes(userResponse);
+}
+
+console.log(playGame());
+
+*/
